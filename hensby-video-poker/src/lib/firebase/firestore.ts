0d5bbd2f0f81551c773c108c49b6ef816f3cb8ae
henrythedev90 <henrythedev90@gmail.com>
2025-09-14 20:28:26 -0400

    updated the sensitive information from the repo

--- hensby-video-poker/src/lib/firebase/firestore.ts
+++ hensby-video-poker/src/lib/firebase/firestore.ts
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCLypDzYu1wFoix0l7OQTPdCg9psH4Le9w",
-  authDomain: "hensbys-video-poker.firebaseapp.com",
-  projectId: "hensbys-video-poker",
-  storageBucket: "hensbys-video-poker.firebasestorage.app",
-  messagingSenderId: "369437016786",
-  appId: "1:369437016786:web:dd4b8d400c2ea389d3230b",
-  measurementId: "G-NLQQK16E34",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
